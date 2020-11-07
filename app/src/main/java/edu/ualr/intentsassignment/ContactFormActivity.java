0fcc034479c2e8bfd2232681413776418fa1f43c
lgnowlin <lgnowlin@ualr.edu>
2020-11-07 16:38:32 -0600

    Final Commit

--- app/src/main/java/edu/ualr/intentsassignment/ContactFormActivity.java
+++ app/src/main/java/edu/ualr/intentsassignment/ContactFormActivity.java
@@ -3,6 +3,8 @@ package edu.ualr.intentsassignment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +21,12 @@ public class ContactFormActivity extends AppCompatActivity {
     public static final String EXTRA_CONTACT = "ContactData";
     private ActivityFormBinding mBinding;
     private MaterialButton saveBtn;
+    public EditText firstName;
+    public EditText lastName;
+    public EditText phoneNumber;
+    public EditText emailAddress;
+    public EditText address;
+    public EditText website;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,6 +34,12 @@ public class ContactFormActivity extends AppCompatActivity {
         mBinding = ActivityFormBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
         saveBtn = findViewById(R.id.saveButton);
+        firstName = findViewById(R.id.editFirstName);
+        lastName = findViewById(R.id.editLastName);
+        phoneNumber = findViewById(R.id.editPhoneNumber);
+        emailAddress = findViewById(R.id.editEmailAddress);
+        address = findViewById(R.id.editAddress);
+        website = findViewById(R.id.editWebsite);
 
         this.saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,7 +50,7 @@ public class ContactFormActivity extends AppCompatActivity {
     }
     public void onButtonClick(View view){
         Intent intent = new Intent(this, ContactInfoActivity.class);
-        Contact c = new Contact("Gracie", "Nowlin", "501-519-1294", "lgnowlin@ualr.edu", "7 Cozy Lane, Redfield, AR", "www.ualr.edu");
+        Contact c = new Contact(firstName.getText().toString(), lastName.getText().toString(), phoneNumber.getText().toString(), emailAddress.getText().toString(), address.getText().toString(), website.getText().toString());
         intent.putExtra(EXTRA_CONTACT, c);
         startActivity(intent);
     }
